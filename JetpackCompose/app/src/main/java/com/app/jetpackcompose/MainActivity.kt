ecffdaef9fa755b167d60896b90a153e5cef5e7b
Rupkuvarba <baradrupce@gmail.com>
2022-11-04 15:26:14 +0530

    Jetpack compose - Dynamic content example - List

--- JetpackCompose/app/src/main/java/com/app/jetpackcompose/MainActivity.kt
+++ JetpackCompose/app/src/main/java/com/app/jetpackcompose/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
@@ -16,6 +18,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+val namesList:List<String> = listOf(
+    "Bhavin",
+    "Chirag",
+    "Rup",
+    "mital",
+    "Ganpati")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,25 +38,29 @@ class MainActivity : ComponentActivity() {
             }
         }*/
         setContent {
-            MaterialTheme {
-                Greeting(name = "Android \n\nHi Rup!!\n\nWelcome on the board")
-               // Greeting()
+            Column() {
+                MaterialTheme {
+                    Greeting(name = "Android \n\nHi Rup!!\n\nWelcome on the board")
+                    // Greeting()
+                }
+                GreetingList(names = namesList)
             }
+
         }
     }
 
-
     @Composable
     fun Greeting(name: String) {
-        //Text(text = "Hello $name!")
-        Text(name,
+        Text(text = "Hello $name!",
+        style = MaterialTheme.typography.h4)
+        /*Text(name,
             Modifier
                 .padding(10.dp)
-                .border(2.dp,Color.Green, RoundedCornerShape(50))
+                .border(2.dp, Color.Green, RoundedCornerShape(50))
                 .padding(50.dp)
                 .border(2.dp, Color.Red)
                 .padding(50.dp)
-        )
+        )*/
     }
 
     @Preview(showBackground = true)
@@ -55,6 +68,21 @@ class MainActivity : ComponentActivity() {
     fun DefaultPreviewMainActivity() {
         MaterialTheme {
             Greeting( "SiddhaRajsinh !")
+        }
+    }
+
+    @Composable
+    fun GreetingList(names: List<String>) {
+        //Text(text = "Hello $name!")
+        Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+           for (name in names){
+               Greeting(name = name)
+           }
+            Button(onClick = { /*namesList.add("new name")*/ }) {
+              Text(text = "Add new name")
+            }
         }
     }
 }
---
+++ JetpackCompose/document/readme.md
@@ -0,0 +1,3 @@
+The doc file is on below link 
+
+https://docs.google.com/document/d/11I94ixz1qC3b33R9drlY-cEDcCpEi4GrnMfILfA3tIg/edit#
